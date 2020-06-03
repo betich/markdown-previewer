d62ef178ffb04b13f3a7d1bfe2ca1980ba46a3b6
Panithi Makthiengtrong <betich365@gmail.com>
2020-06-04 03:22:43 +0700

    Add Markdown conversion system and app structure

---
+++ src/components/Editor.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import marked from 'marked';
+
+class Preview extends React.Component {
+    constructor(props) {
+        super(props);
+        this.props = props;
+    }
+
+    render() {
+        return (
+            <div className="panelArea">
+                <div className="topbar">
+                    <label htmlFor="preview">Preview</label>
+                </div>
+                <div id="preview" dangerouslySetInnerHTML={{__html: this.props.html}}></div>
+            </div>
+        );
+    }
+}
+
+class MarkdownPanels extends React.Component {
+    constructor(props) {
+        super(props);
+        this.props = props;
+        let placeholder = "# Markdown Previewer\r\n\n" 
+                        + "[My GitHub](https://www.github.com/betich \"My GitHub\")\r\n\n"
+                        + "![Random Image](https://images.pexels.com/photos/4321944/pexels-photo-4321944.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260 \"Random Image\")"
+                        + "\n----\n## Markdown quick reference\r\n\n"
+                        + "# headers\r\n\n"
+                        + "*emphasis*\r\r\n"
+                        + "**strong**\r\r\n"
+                        + "* list\n* list\r\n\n1. list\n2. list\r\n\n"
+                        + ">blockquote\r\r\n"
+                        + "```\nfor var(i=0; i<10; i++) {\r\n"
+                        + "\tconsole.log(i);\r\n"
+                        + "}\n```\r\n\n"
+                        + "Inline `code` (backticks)\r\n\n"
+                        + "[links](https://wikipedia.org)";
+        this.state = {
+            input: placeholder
+        }
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({
+            input: event.target.value
+        });
+    }
+
+    render() {
+        return(
+            <div>
+                <div className="panelArea">
+                    <div className="topbar">
+                        <label htmlFor="editor">Markdown</label>
+                    </div>
+                    <textarea id="editor" rows="10" value={this.state.input} onChange={this.handleChange} placeholder="Markdown Input"></textarea>
+                </div>
+                <Preview html={marked(this.state.input, {breaks: true})} />
+            </div>
+        );
+    }
+}
+
+export default MarkdownPanels;
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header className="App-header">
+            <h1>Markdown Previewer</h1>
+            <a className="App-link" href="https://www.github.com/betich" target="_blank" rel="noopener noreferrer">
+              Check me out
+            </a>
+        </header>
+    );
+}
+
+export default Header;
